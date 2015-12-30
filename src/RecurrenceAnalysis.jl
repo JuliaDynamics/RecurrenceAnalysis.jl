5b5af446b72f5f9ba3aa9c4cd25f707f79e038f4
heliosdrm <helios.derosario@gmail.com>
2015-12-30 01:51:42 +0100

    Fix types in functions; add Marwan's definition of trend

--- src/RecurrenceAnalysis.jl
+++ src/RecurrenceAnalysis.jl
@@ -5,7 +5,8 @@ export RecurrenceMatrix
 # vnorm function can be redefined
 vnorm = x -> norm(x, Inf)
 
-function RecurrenceMatrix(x::Array{Float64,1}, d::Int, k::Int, radius::Float64)
+function RecurrenceMatrix(x::Array{Float64,1}, d::Integer, k::Integer, radius::Real)
+    halfradius = 0.5radius
     n = length(x)
     # Create k-vectors with delay d
     # Each k-vector is in a row
@@ -25,7 +26,7 @@ function RecurrenceMatrix(x::Array{Float64,1}, d::Int, k::Int, radius::Float64)
         match_all = false
         if dq < radius
             rmat[p,q] = rmat[q,p] = true
-            match_all = (dq < 0.5radius)
+            match_all = (dq < halfradius)
         end
         # Continue with following points
         while ((q+=1) <= nk) && (xk_sort[q,1] - xk_sort[p,1] < radius)
@@ -33,7 +34,7 @@ function RecurrenceMatrix(x::Array{Float64,1}, d::Int, k::Int, radius::Float64)
             if dq < radius
                 rmat[p, q] = rmat[q, p] = true
                 # Mark all crossings within half radius
-                if match_all && dq < 0.5radius
+                if match_all && dq < halfradius
                     rmat[q0:q-1, q] = true
                     rmat[q, q0:q-1] = true
                     first_neighbour[q0:q-1] = q+1
@@ -53,10 +54,10 @@ function RecurrenceMatrix(x::AbstractVecOrMat, d::Real, k::Real, radius::Real)
     if length(x) != maximum(size(x))
         error("`x` must be a row or column vector")
     end
-    RecurrenceMatrix(float(x[:]), Int(d), Int(k), float(radius))
+    RecurrenceMatrix(float(x[:]), Int(d), Int(k), radius)
 end
 
-function RecurrenceMatrixBruteForce(x::Array{Float64,1}, d::Int, k::Int, radius::Float64)
+function RecurrenceMatrixBruteForce(x::Array{Float64,1}, d::Integer, k::Integer, radius::Real)
     n = length(x)
     # Create k-vectors with delay d
     # Each k-vector is in a row
@@ -79,22 +80,23 @@ end
 # The optimized algorithm is about 3 times faster and takes about 9.5 times less
 # memory than the brute force algorithm, for a matrix with sparsity ratio ~ 0.06
 
-function diagonal_bins(x::SparseMatrixCSC{Bool})
+function diagonalstructure(x::SparseMatrixCSC{Bool})
     bins = [0]
     nbins = 1
     current_diag = 0
     n = minimum(size(x))
+    npoints = zeros(n-1)
     # Iterate over diagonals
     for d = 1:n-2
         previous_cell = false
-        for r = 1:n-d
+        for c = d+1:n
             # Operate on existing diagonal line
             if previous_cell
                 # Extend length with current cell
-                (extend = x[r,r+d]) && (current_diag += 1)
+                (extend = x[c-d,c]) && (current_diag += 1)
                 # If arrived to the end of a line
                 # add the current length to the corresponding bin
-                if (!extend || (r == n-d)) && (current_diag > 0)
+                if (!extend || (c == n)) && (current_diag > 0)
                     # Append new positions to the bins if needed
                     if current_diag > nbins
                         append!(bins, zeros(current_diag - nbins))
@@ -104,11 +106,13 @@ function diagonal_bins(x::SparseMatrixCSC{Bool})
                     current_diag = 0
                 end
             end
-            previous_cell = x[r,r+d]
+            (previous_cell = x[c-d,c]) && (npoints[d] += 1) 
         end
     end
     # Add isolated points in first bin
-    [nnz(triu(x,1)) - sum(bin); bins]
+    bins = [nnz(triu(x,1)) - collect(2:nbins+1)'*bins; bins]
+    npoints[n-1] = (x[1,n] ? 1 : 0)
+    (bins, npoints)
 end
 
 recurrence_rate(x::SparseMatrixCSC{Bool}) = nnz(x)/prod(size(R))
@@ -131,7 +135,7 @@ maxdiag(x::SparseMatrixCSC{Bool}) = maxdiag(diagonal_bins(x))
 
 divergence(x) = 1/maxdiag(x)
 
-function entropy(diag_bins::Array{Int}, lmin::Int)
+function entropy(diag_bins::Array{Integer}, lmin::Int)
     if lmin < 2
         error("lmin must be 2 or higher")
     end
@@ -142,4 +146,15 @@ function entropy(diag_bins::Array{Int}, lmin::Int)
     else
         0.0
     end
+end
+
+# Marwan
+function trend(npoints::Integer, border::Integer)
+  nmax = length(npoints)
+  rrk = npoints./collect(nmax:-1:1)
+  m = n-border
+  w = (1:m-m/2)
+  w'*(rrk[1:m]-mean(rrk[1:m])) / (w'*w)
+end
+
 end
