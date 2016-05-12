98fc0d1680e1e56571740bf98193a40d02dfe930
Helios De Rosario <helios.derosario@gmail.com>
2016-05-13 00:09:15 +0200

    Modify FNN algorithms

--- src/dimension.jl
+++ src/dimension.jl
@@ -11,78 +11,77 @@ function embedmatrix(x, m, delay, metric="max")
     dist = getmetric(metric)
     fun = Dict(Euclidean()=>+, Chebyshev()=>max)[dist]
     nr, nc = size(x)
-    nrm = nr - delay*(m-1)
-    ncm = nc - delay*(m-1)
+    nrm = nr - delay*m
+    ncm = nc - delay*m
     x = abs2(x)
     xm = x[1:nrm, 1:ncm]
-    for k = 1:m-1
+    for k = 1:m
         xm = fun(xm, x[delay*k+(1:nrm), delay*k+(1:ncm)])
     end
     sqrt(xm)
 end
 
 # Kennel's algorithm
-function fnn(x, m1, m2, delay, thresholds; metric="max")
+function fnn(x, mbounds, delay, thresholds; metric="max")
     Rtol2 = thresholds[1]^2
     Atol = thresholds[2]
     Ra = std(x)
+    m1, m2 = mbounds
     nfnn = zeros(m2-m1+1)
     # DM - with maximum value in the diagonal to rule it out in neighbour search
-    dm = distancematrix(x)
-    n = size(x)[1]
-    dm[1:n+1:end] = maximum(dm)
     # Start with matrix embedded at m=m1
-    dme = embedmatrix(dm, m1, delay, metric)
-    nm = n - delay*m2
+    dm = distancematrix(embed(x, m1, delay), metric)
+    n = size(dm)[1]
+    dm[1:n+1:end] = maximum(dm)
     for m = 1:length(nfnn)
-        # Look for nearest neighbours within the first nm points
-        nnval, nnpos = findmin(dme[1:nm,1:nm], 2)
-        dme = embedmatrix1(dme, delay, metric)
-        nnv1 = nnval[1:nm]
-        nnv2 = dme[1:nm,1:nm][nnpos]
-        fnn1 = ( (nnv2./nnv1-1) .> Rtol2 )
+        # Look for nearest neighbours within the first n-delay
+        n -= delay
+        nnval, nnpos = findmin(dm[1:n,1:n], 2)
+        dm = embedmatrix1(dm, delay, metric)
+        nnv1 = nnval[1:n]
+        nnv2 = dm[1:n,1:n][nnpos]
+        fnn1 = ( ((nnv2.^2)./(nnv1.^2)-1) .> Rtol2 )
         fnn2 = ( (nnv2/Ra) .> Atol )
         nfnn[m] = sum(fnn1 | fnn2)
     end
+    nfnn
 end
 
 # Cao's algorithm
-function afnn(x, m1, m2, delay; metric="max")
-    dm = distancematrix(x)
-    n = size(x)[1]
+function afnn(x, mbounds, delay; metric="max")
+    m1, m2 = mbounds
+    dm = distancematrix(embed(x, m1, delay), metric)
+    n = size(dm)[1]
     dm[1:n+1:end] = maximum(dm)
-    dme = embedmatrix(dm, m1, delay, metric)
-    mn = size(dme)[1]
     mean_increment = zeros(m2-m1+2)
     mean_ratio = zeros(m2-m1+2)
     for m = 1:length(mean_ratio)
-        nm -= delay
-        nnval, nnpos = findmin(dme[1:nm,1:nm], 2)
-        dme = embedmatrix1(dme, delay, metric)
-        nnv1 = nnval[1:nm]
-        nnv2 = dme[1:nm,1:nm][nnpos]
-        mean_increment[m] = nnv2 - nnv1
-        mean_ratio[m] = mean(nnv2./nnv2)
+        n -= delay
+        nnval, nnpos = findmin(dm[1:n,1:n], 2)
+        dm = embedmatrix1(dm, delay, metric)
+        nnv1 = nnval[1:n]
+        nnv2 = dm[1:n,1:n][nnpos]
+        mean_ratio[m] = mean(nnv2./nnv1)
+        mean_increment[m] = mean(sqrt(nnv2.^2 - nnv1.^2))
     end
-    e1 = mean_increment[2:end]./mean_increment[1:end-1]
-    e2 = mean_ratio[2:end]./mean_ratio[1:end-1]
+    e1 = mean_ratio[2:end]./mean_ratio[1:end-1]
+    e2 = mean_increment[2:end]./mean_increment[1:end-1]
     e1, e2
 end
 
 # Krakovská's algorithm
-function ffnn(x, m1, m2, delay; metric="max")
-    dm = distancematrix(x)
+function ffnn(x, bounds, delay; metric="max")
+    m1, m2 = mbounds
+    dm = distancematrix(embed(x, m1, delay), metric)
     n = size(x)[1]
     dm[1:n+1:end] = maximum(dm)
-    dme = embedmatrix(dm, m1, delay, metric)
-    nm = size(dme)[1]
     fnn_ratio = zeros(m2-m1+1)
-    for m = 1:length(mean_ratio)
-        nm -= delay
-        nnval, nnpos1 = findmin(dme[1:nm,1:nm], 2)
-        dme = embedmatrix1(dme, delay, metric)
-        nnval, nnpos2 = findmin(dme, 2)
-        ffn_ratio[m] = sum(nnpos1 .!= nnpos2)/nm
+    for m = 1:length(fnn_ratio)
+        n -= delay
+        nnval, nnpos1 = findmin(dm[1:n,1:n], 2)
+        dm = embedmatrix1(dm, delay, metric)
+        nnval, nnpos2 = findmin(dm, 2)
+        fnn_ratio[m] = sum(nnpos1 .!= nnpos2)/n
     end
-    ffn_ratio
+    fnn_ratio
 end
