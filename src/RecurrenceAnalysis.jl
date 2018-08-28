666739d7b57ded44ae01fc48f5f575ad94e1a029
Helios De Rosario <helios.derosario@gmail.com>
2018-08-28 16:27:43 +0200

    fixed recurrence rate and cityblock distance
    bugfix of warnings

--- src/RecurrenceAnalysis.jl
+++ src/RecurrenceAnalysis.jl
@@ -34,7 +34,10 @@ function getmetric(normtype::AbstractString)
     metrics = Dict(
         "euclidean"=>Euclidean(),
         "max"=>Chebyshev(),
-        "inf"=>Chebyshev()
+        "inf"=>Chebyshev(),
+        "cityblock"=>Cityblock(),
+        "manhattan"=>Cityblock(),
+        "taxicab"=>Cityblock()
         )
     !haskey(metrics,normtype) && error("incorrect norm type. Accepted values are \""
         *join(keys(metrics),"\", \"", "\" or \"") * "\".")
@@ -59,7 +62,7 @@ function embed(x::AbstractVecOrMat, m::Integer, delay::Integer)
     dims = size(x)
     n = dims[1]
     nm = n-delay*(m-1)
-    (nm < 2) && warning("the emedded time series has length < 2")
+    (nm < 2) && @warn "the embedded time series has length < 2"
     ix = (1:nm) .+ (0:delay:delay*(m-1))'
     embed_indices(x, ix)
 end
@@ -85,7 +88,8 @@ end
     
 Create a distance matrix from one or two (possibly embedded) time series.
 
-Available metrics for the distances are `"max"` (default), `"inf"` (same), and `"euclidean"`.
+Available metrics for the distances are `"max"` (default), `"inf"` (same),
+`"euclidean"`, or `"manhattan"` - also known as `"cityblock"` or `"manhattan"`.
 """
 function distancematrix(x::AbstractVecOrMat, metric::AbstractString="max")
     dist = getmetric(metric)
@@ -106,12 +110,20 @@ Create a recurrence matrix from an embedded time series.
 * `x::Any` : embedded time series.
 * `radius::Any` : threshold parameter to classify distances as recurrences.
 * `scale::Any` : function of the distance matrix, or fixed number to scale the distances
+* `fixedrate::Real` : a number between 0 and 1 to define a fixed recurrence rate.
+If a valid number is given, the radius and the scale are ignored.
 between points. Typical choices are `maximum` to scale distances into
 the unit inteval, or `mean`. Use `1` to keep the distances unscaled (default).
 * `metric::String` : metric of the norm, as in `distancematrix`.
 """
 function recurrencematrix(x, radius; scale=1, kwargs...)
     kwargs = Dict(kwargs)
+    # check fixed recurrence rate within (0,1)
+    p = pop!(kwargs, :fixedrate, -1)
+    if 0 <= p <= 1
+        sfun = (m) -> quantile(m[:], p)
+        return recurrencematrix(x, 1; scale=sfun, kwargs...)
+    end
     argsdm = haskey(kwargs,:metric) ? (x, kwargs[:metric]) : (x,)
     dm = distancematrix(argsdm...)
     (typeof(scale) <: Function) && (scale = scale(dm))
@@ -127,6 +139,12 @@ See `?recurrencematrix` for details.
 """
 function crossrecurrencematrix(x, y, radius; scale=1, kwargs...)
     kwargs = Dict(kwargs)
+    # check fixed recurrence rate within (0,1)
+    p = pop!(kwargs, :fixedrate, -1)
+    if 0 <= p <= 1
+        sfun = (m) -> quantile(m[:], p)
+        return crossrecurrencematrix(x, y, 1; scale=sfun, kwargs...)
+    end
     argsdm = haskey(kwargs,:metric) ? (x, y, kwargs[:metric]) : (x, y)
     dm = distancematrix(argsdm...)
     (typeof(scale) <: Function) && (scale = scale(dm))
