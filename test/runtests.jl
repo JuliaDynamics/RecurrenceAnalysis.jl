aece1e8dcf007fe774e33b21dde1748c7ca23513
Helios De Rosario <helios.derosario@gmail.com>
2019-01-15 23:56:44 +0100

    extended test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,54 +1,5 @@
 using RecurrenceAnalysis
-using Test, DynamicalSystemsBase
+using Test
 
-systems = (Systems.lorenz(),)
-
-testvalues = (
-[426/234^2, 364/426, 364/50, 23, 2.02058292, 3.21794972e-7, 92/426, 92/30, 4],
-)
-
-u0s = (ones(3),)
-
-dt = 0.01
-for (ds, vals, u0) in zip(systems, testvalues, u0s)
-
-    data = trajectory(ds, dt*1000, u0; dt = dt)
-    x = data[501:2:end,1]
-
-    # Look for optimal threshold
-    dd, rr = sorteddistances(x, theiler=1)
-    # Distance and recurrence matrices
-    xe = embed(x, 3, 8)
-    rmat = RecurrenceMatrix(xe, 1.5, metric="max")
-    y = data[701:2:end,3]
-    crmat = CrossRecurrenceMatrix(x, y, 1.5)
-    jrmat = JointRecurrenceMatrix(x, y, 1.5)
-    # Recurrence plot
-    crp = recurrenceplot(crmat, width=125)
-    @test size(crp)[1] == 75
-    # RQA
-    rqapar = rqa(rmat, theiler=2, lmin=3, border=20)
-    tol = 1e-5
-    @test rqapar["RR"] ≈ vals[1]    atol = tol
-    @test rqapar["DET"] ≈ vals[2]   atol = tol
-    @test rqapar["L"] ≈ vals[3]     atol = tol
-    @test rqapar["Lmax"] ≈ vals[4]  atol = tol
-    @test rqapar["ENT"] ≈ vals[5]   atol = tol
-    @test rqapar["TND"] ≈ vals[6]   atol = tol
-    @test rqapar["LAM"] ≈ vals[7]   atol = tol
-    @test rqapar["TT"] ≈ vals[8]    atol = tol
-    @test rqapar["Vmax"] ≈ vals[9]  atol = tol
-    rqadiag = rqa(rmat, theiler=2, lmin=3, border=20, onlydiagonal=true)
-    @test all([rqapar[p]==rqadiag[p] for p in keys(rqadiag)])
-    # Fixed rate for recurrence matrix
-    rmat2 = RecurrenceMatrix(xe[1:3:end,:], 0.05; fixedrate=true)
-    @test .049 < recurrencerate(rmat2) < .051
-    # Windowed RQA
-    rmatw = @windowed RecurrenceMatrix(xe, 1.5, metric=RecurrenceAnalysis.Chebyshev()) 50
-    crmatw = @windowed(CrossRecurrenceMatrix(x, y, 1.5),30)
-    @windowed jrmatw = JointRecurrenceMatrix(x, y, 1.5) 30
-    @test jrmatw[33 .+ (1:30), 33 .+ (1:30)] == jrmat[33 .+ (1:30), 33 .+ (1:30)]
-    @windowed(rrw = recurrencerate(rmatw), width=50, step=40)
-    @windowed rqaw = rqa(rmatw) width=50 step=40
-    @test rqaw["RR"] == rrw
-end
+include("dynamicalsystems.jl")
+include("smallmatrix.jl")
---
+++ test/smallmatrix.jl
@@ -0,0 +1,108 @@
+using RecurrenceAnalysis, SparseArrays
+using Test
+
+cells = [(1,1),(2,2),(3,2),(6,2),(7,2),(8,2),(6,3),(7,3),(10,3),(2,4),(4,4),
+    (1,5),(2,5),(3,5),(5,5),(9,5),(10,5),(2,6),(3,6),(6,6),(7,6),(10,6),
+    (3,7),(8,7),(4,8),(1,9),(5,9),(7,9),(9,9),(9,10),(10,10),(3,11),(7,11)]
+i = [x[1] for x in cells]
+j = [x[2] for x in cells]
+rmat = CrossRecurrenceMatrix(sparse(i,j,trues(length(i))))
+### Graph of the recurrence matrix:
+## · : non-recurrent (white) points
+## x : recurrent (black) points
+## o : recurrent points within Theiler window = 2
+#
+#    1 2 3 4 5 6 7 8 9 A B
+#  1 o · · · x · · · x · ·
+#  2 · o · x x x · · · · ·
+#  3 · o · · x x x · · · x
+#  4 · · · o · · · x · · ·
+#  5 · · · · o · · · x · ·
+#  6 · x x · · o · · · · ·
+#  7 · x x · · o · · x · x
+#  8 · x · · · · o · · · ·
+#  9 · · · · x · · · o o ·
+#  A · · x · x x · · · o · 
+#
+###
+
+@testset "Recurrence structures" begin
+    @testset "Default parameters" begin
+        histograms = recurrencestructures(rmat)
+        true_histograms = Dict("diagonal" => [11,7,1,0,1],
+                               "vertical" => [15,6,2],
+                               "recurrencetimes" => [3,1,4,4])
+        for k in keys(histograms)
+            @test histograms[k] == true_histograms[k]
+        end
+        rqa_params = rqa(rmat, theiler=0, lmin=1)
+        @test rqa_params["RR"] == 33/110
+        @test rqa_params["DET"] == 1.0
+        @test rqa_params["L"] == 33/20
+        @test rqa_params["Lmax"] == 5
+        @test rqa_params["DIV"] == 0.2
+        @test rqa_params["ENTR"] ≈ 0.996 atol=0.001
+        @test rqa_params["LAM"] == 1.0
+        @test rqa_params["TT"] == 33/23
+        @test rqa_params["Vmax"] == 3
+    end
+    @testset "With Theiler window" begin
+        histograms = recurrencestructures(rmat, theiler=2)
+        true_histograms = Dict("diagonal" => [9,4,0,0,1],
+                               "vertical" => [10,3,2],
+                               "recurrencetimes" => [1,0,3,0,0,0,2])
+        for k in keys(histograms)
+            @test histograms[k] == true_histograms[k]
+        end
+        rqa_params = rqa(rmat, theiler=2, lmin=1)
+        @test rqa_params["RR"] == 22/110
+        @test rqa_params["DET"] == 1.0
+        @test rqa_params["L"] == 22/14
+        @test rqa_params["Lmax"] == 5
+        @test rqa_params["DIV"] == 0.2
+        @test rqa_params["ENTR"] ≈ 0.830 atol=0.001
+        @test rqa_params["LAM"] == 1.0
+        @test rqa_params["TT"] == 22/15
+        @test rqa_params["Vmax"] == 3
+    end
+    @testset "With minimum line" begin
+        histograms = recurrencestructures(rmat, lmin=2)
+        true_histograms = Dict("diagonal" => [0,7,1,0,1],
+                               "vertical" => [0,6,2],
+                               "recurrencetimes" => [0,0,0,2,0,0,1])
+        for k in keys(histograms)
+            @test histograms[k] == true_histograms[k]
+        end
+        rqa_params = rqa(rmat, theiler=0, lmin=2)
+        @test rqa_params["RR"] == 33/110
+        @test rqa_params["DET"] == 22/33
+        @test rqa_params["L"] == 22/9
+        @test rqa_params["Lmax"] == 5
+        @test rqa_params["DIV"] == 0.2
+        @test rqa_params["ENTR"] ≈ 0.684 atol=0.001
+        @test rqa_params["LAM"] == 18/33
+        @test rqa_params["TT"] == 18/8
+        @test rqa_params["Vmax"] == 3
+    end
+    @testset "Theiler and minimum line" begin
+        histograms = recurrencestructures(rmat, theiler=2, lmin=2)
+        true_histograms = Dict("diagonal" => [0,4,0,0,1],
+                               "vertical" => [0,3,2],
+                               "recurrencetimes" => [0,0,0,0,0,0,1])
+        for k in keys(histograms)
+            @test histograms[k] == true_histograms[k]
+        end
+        rqa_params = rqa(rmat, theiler=2, lmin=2)
+        @test rqa_params["RR"] == 22/110
+        @test rqa_params["DET"] == 13/22
+        @test rqa_params["L"] == 13/5
+        @test rqa_params["Lmax"] == 5
+        @test rqa_params["DIV"] == 0.2
+        @test rqa_params["ENTR"] ≈ 0.500 atol=0.001
+        @test rqa_params["LAM"] == 12/22
+        @test rqa_params["TT"] == 12/5
+        @test rqa_params["Vmax"] == 3
+    end
+end
+
+
