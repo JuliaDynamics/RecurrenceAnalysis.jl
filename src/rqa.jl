ba789648777013fd85f66b7419bf29c511c0e7c7
George Datseris <datseris.george@gmail.com>
2019-01-23 20:36:57 +0100

    change all x to R in rqa docs
    for documentation purposes

--- src/rqa.jl
+++ src/rqa.jl
@@ -3,19 +3,19 @@
 # Recurrence rate
 
 """
-    recurrencerate(x[; theiler])
+    recurrencerate(R[; theiler])
 
-Calculate the recurrence rate of the recurrence matrix `x`.
+Calculate the recurrence rate of the recurrence matrix `R`.
 
 ## Description
 
 The recurrence rate is calculated as:
 
 ```math
-RR = \\frac{1}{S} \\sum_{i,j} x[i,j]
+RR = \\frac{1}{S} \\sum_{i,j} R[i,j]
 ```
 
-where *S* is the size of `x` or the region of `x` with potential recurrent points.
+where *S* is the size of `R` or the region of `R` with potential recurrent points.
 There is not a unique definition of that denominator, which is defined as the
 full size of the matrix in many sources (e.g. [1]), whereas
 in others it is adjusted to remove the points of the LOI when they are
@@ -50,28 +50,28 @@ URL: https://www.nsf.gov/pubs/2005/nsf05057/nmbs/nmbs.pdf
 recurrence quantifications", in: Webber, C.L. & N. Marwan (eds.), *Recurrence
 Quantification Analysis. Theory and Best Practices*, Springer, pp. 3-43 (2015).
 """
-function recurrencerate(x::ARM; theiler::Integer=deftheiler(x), kwargs...)::Float64
+function recurrencerate(R::ARM; theiler::Integer=deftheiler(R), kwargs...)::Float64
     (theiler < 0) && throw(ErrorException(
         "Theiler window length must be greater than or equal to 0"))
     if theiler == 0
-        return nnz(x)/length(x)
+        return nnz(R)/length(R)
     end
     diags_remove = -(theiler-1):(theiler-1)
     theiler_nz = 0
     for d in diags_remove
-        theiler_nz += nnz(diag(x,d))
+        theiler_nz += nnz(diag(R,d))
     end
-    return (nnz(x) - theiler_nz)/_rrdenominator(x; theiler=theiler, kwargs...)
+    return (nnz(R) - theiler_nz)/_rrdenominator(R; theiler=theiler, kwargs...)
 end
 
 # Calculate the denominator for the recurrence rate
-_rrdenominator(x::ARM; theiler=0, kwargs...) = length(x)
+_rrdenominator(R::ARM; theiler=0, kwargs...) = length(R)
 
-function _rrdenominator(x::M; theiler=0, kwargs...) where
+function _rrdenominator(R::M; theiler=0, kwargs...) where
     M<:Union{RecurrenceMatrix,JointRecurrenceMatrix}
 
-    (theiler == 0) && (return length(x))
-    k = size(x,1) - theiler
+    (theiler == 0) && (return length(R))
+    k = size(R,1) - theiler
     return k*(k+1)
 end
 
@@ -105,16 +105,16 @@ macro histogram_params(keyword, description, hist_fun)
         _fname = Symbol("_$(keyword)_$(name)")
         fbody = function_bodies[name]
         doc = """
-                $fname(x[; lmin=2, theiler])
+                $fname(R[; lmin=2, theiler])
 
-            Calculate the $(param) contained in the recurrence matrix `x`,
+            Calculate the $(param) contained in the recurrence matrix `R`,
             ruling out the lines shorter than `lmin` (2 by default) and all the
             points inside the Theiler window (see [`rqa`](@ref) for the
             default values and usage of the keyword argument `theiler`).
             """
         push!(ret.args, quote
             @doc $doc ->
-            $fname(x::ARM; kwargs...) = $_fname($hist_fun(x; kwargs...))
+            $fname(R::ARM; kwargs...) = $_fname($hist_fun(R; kwargs...))
 
             function $_fname(hist::Vector{<:Integer})
                 $fbody
@@ -130,9 +130,9 @@ end
 @histogram_params dl "diagonal line" diagonalhistogram
 
 """
-    determinism(x[; lmin=2, theiler])
+    determinism(R[; lmin=2, theiler])
 
-Calculate the determinism of the recurrence matrix `x`:
+Calculate the determinism of the recurrence matrix `R`:
 
 ## Description
 
@@ -149,9 +149,9 @@ is the number of lines of length equal to ``l``.
 points inside the Theiler window (see [`rqa`](@ref) for the
 default values and usage of the keyword argument `theiler`).
 """
-function determinism(x::ARM; kwargs...)
-    npoints = recurrencerate(x; kwargs...)*_rrdenominator(x; kwargs...)
-    return _determinism(diagonalhistogram(x; kwargs...), npoints)
+function determinism(R::ARM; kwargs...)
+    npoints = recurrencerate(R; kwargs...)*_rrdenominator(R; kwargs...)
+    return _determinism(diagonalhistogram(R; kwargs...), npoints)
 end
 
 function _determinism(diag_hist::Vector{<:Integer}, npoints)::Float64
@@ -162,18 +162,18 @@ end
 
 
 """
-    divergence(x[; theiler])
+    divergence(R[; theiler])
 
-Calculate the divergence of the recurrence matrix `x`
+Calculate the divergence of the recurrence matrix `R`
 (actually the inverse of [`dl_max`](@ref)).
 """
-divergence(x::ARM; kwargs...) = ( 1.0/dl_max(x; kwargs...) )
+divergence(R::ARM; kwargs...) = ( 1.0/dl_max(R; kwargs...) )
 
 
 """
-    trend(x[; border=10, theiler])
+    trend(R[; border=10, theiler])
 
-Calculate the trend of recurrences in the recurrence matrix `x`.
+Calculate the trend of recurrences in the recurrence matrix `R`.
 
 ## Description
 
@@ -204,15 +204,15 @@ originate them are not of the same length), the limits of the formula for TREND
 are not clearly defined. For the sake of consistency, this function limits the
 calculations to the biggest square matrix that contains the LOI.
 """
-trend(x::ARM; theiler=deftheiler(x), kwargs...) =
-    _trend(tau_recurrence(x); theiler=theiler, kwargs...)
+trend(R::ARM; theiler=deftheiler(R), kwargs...) =
+    _trend(tau_recurrence(R); theiler=theiler, kwargs...)
 
-function tau_recurrence(x::ARM)
-    n = minimum(size(x))
-    rv = rowvals(x)
+function tau_recurrence(R::ARM)
+    n = minimum(size(R))
+    rv = rowvals(R)
     rr_τ = zeros(n)
     @inbounds for col=1:n
-        for i in nzrange(x, col)
+        for i in nzrange(R, col)
             if (r=rv[i]) ≤ n
                 d = abs(r-col)
                 if d==0
@@ -241,7 +241,7 @@ function _trend(rr_τ::Vector; theiler=1, border=10, kwargs...)::Float64
 end
 
 # Number of l-length sequences, based on diagonals
-# countsequences(x::ARM; kwargs...) = countsequences(diagonalhistogram(x; kwargs...))
+# countsequences(R::ARM; kwargs...) = countsequences(diagonalhistogram(R; kwargs...))
 #
 # function countsequences(diag_hist::Vector; lmin=2, kwargs...)
 #     overlap = (1:length(diag_hist))' .- (lmin+1)
@@ -256,9 +256,9 @@ end
 
 
 """
-    laminarity(x[; lmin=2, theiler])
+    laminarity(R[; lmin=2, theiler])
 
-Calculate the laminarity of the recurrence matrix `x`.
+Calculate the laminarity of the recurrence matrix `R`.
 
 ## Description
 
@@ -275,17 +275,17 @@ is the number of lines of length equal to ``v``.
 points inside the Theiler window (see [`rqa`](@ref) for the
 default values and usage of the keyword argument `theiler`).
 """
-function laminarity(x::ARM; kwargs...)
-    npoints = recurrencerate(x)*_rrdenominator(x; kwargs...)
-    return _laminarity(verticalhistograms(x; kwargs...)[1], npoints)
+function laminarity(R::ARM; kwargs...)
+    npoints = recurrencerate(R)*_rrdenominator(R; kwargs...)
+    return _laminarity(verticalhistograms(R; kwargs...)[1], npoints)
 end
 
 _laminarity(vert_hist::Vector{<:Integer}, npoints) = _determinism(vert_hist, npoints)
 
 """
-    trappingtime(x[; lmin=2, theiler])
+    trappingtime(R[; lmin=2, theiler])
 
-Calculate the trapping time of the recurrence matrix `x`, ruling out the
+Calculate the trapping time of the recurrence matrix `R`, ruling out the
 lines shorter than `lmin` (2 by default) and all the
 points inside the Theiler window (see [`rqa`](@ref) for the
 default values and usage of the keyword argument `theiler`).
@@ -293,7 +293,7 @@ default values and usage of the keyword argument `theiler`).
 The trapping time is the average of the vertical line structures and thus equal
 to [`vl_average`](@ref).
 """
-trappingtime(x::ARM; kwargs...) = vl_average(x; kwargs...)
+trappingtime(R::ARM; kwargs...) = vl_average(R; kwargs...)
 
 
 # 3. Based on recurrence times
@@ -301,20 +301,20 @@ trappingtime(x::ARM; kwargs...) = vl_average(x; kwargs...)
 @histogram_params rt "recurrence time" rt_histogram
 
 """
-    meanrecurrencetime(x[; lmin=2, theiler])
+    meanrecurrencetime(R[; lmin=2, theiler])
 
-Calculate the mean recurrence time of the recurrence matrix `x`, ruling out the
+Calculate the mean recurrence time of the recurrence matrix `R`, ruling out the
 lines shorter than `lmin` (2 by default) and all the
 points inside the Theiler window (see [`rqa`](@ref) for the
 default values and usage of the keyword argument `theiler`).
 
 Equivalent to [`rt_average`](@ref).
 """
-meanrecurrencetime(x::ARM; kwargs...) = rt_average(x; kwargs...)
+meanrecurrencetime(R::ARM; kwargs...) = rt_average(R; kwargs...)
 
 
 """
-    nmprt(x[; lmin=2, theiler])
+    nmprt(R[; lmin=2, theiler])
 
 Calculate the number of the most probable recurrence time (NMPRT), ruling out the
 lines shorter than `lmin` (2 by default) and all the
@@ -331,15 +331,15 @@ of recurrence times [1].
 *Physical Review E*, 75(3), 036222(1-8), 2007, DOI:10.1103/physreve.75.036222
 
 """
-nmprt(x::ARM; kwargs) = maximum(verticalhistograms(x; kwargs...)[2])
+nmprt(R::ARM; kwargs) = maximum(verticalhistograms(R; kwargs...)[2])
 
 
 # 4. All in one
 
 """
-    rqa(x; kwargs...)
+    rqa(R; kwargs...)
 
-Calculate all RQA parameters of a recurrence matrix `x`. See the functions
+Calculate all RQA parameters of a recurrence matrix `R`. See the functions
 referred to below for the definition of
 the different parameters and the default values of the arguments.
 Using this function is much more efficient than calling all individual functions
@@ -402,16 +402,16 @@ in order to restrict the analysis to the recurrence rate and the parameters rela
 to diagonal structures (´RR´, ´DET´, ´L´, ´Lmax´, ´DIV´ and ´ENTR´), which makes
 this function slightly faster.
 """
-function rqa(x; onlydiagonal=false, kwargs...)
+function rqa(R; onlydiagonal=false, kwargs...)
     # Parse arguments for diagonal and vertical structures
     kw_d = Dict(kwargs)
     haskey(kw_d, :theilerdiag) && (kw_d[:theiler] = kw_d[:theilerdiag])
     haskey(kw_d, :lmindiag) && (kw_d[:lmin] = kw_d[:lmindiag])
-    dhist = diagonalhistogram(x; kw_d...)
-    rr_d = recurrencerate(x; kw_d...)
+    dhist = diagonalhistogram(R; kw_d...)
+    rr_d = recurrencerate(R; kw_d...)
     if onlydiagonal
-        return (RR  = recurrencerate(x; kwargs...),
-        DET   = _determinism(dhist, rr_d*_rrdenominator(x; kw_d...)),
+        return (RR  = recurrencerate(R; kwargs...),
+        DET   = _determinism(dhist, rr_d*_rrdenominator(R; kw_d...)),
         L     = _dl_average(dhist),
         Lmax  = _dl_max(dhist),
         DIV   = 1.0/_dl_max(dhist),
@@ -421,16 +421,16 @@ function rqa(x; onlydiagonal=false, kwargs...)
         kw_v = Dict(kwargs)
         haskey(kw_v, :theilervert) && (kw_v[:theiler] = kw_v[:theilervert])
         haskey(kw_v, :lminvert) && (kw_v[:lmin] = kw_v[:lminvert])
-        vhist, rthist = verticalhistograms(x; kw_v...)
-        rr_v = recurrencerate(x; kw_v...)
-        return (RR  = recurrencerate(x; kwargs...),
-            DET  = _determinism(dhist, rr_d*_rrdenominator(x; kw_v...)),
+        vhist, rthist = verticalhistograms(R; kw_v...)
+        rr_v = recurrencerate(R; kw_v...)
+        return (RR  = recurrencerate(R; kwargs...),
+            DET  = _determinism(dhist, rr_d*_rrdenominator(R; kw_v...)),
             L    = _dl_average(dhist),
             Lmax = _dl_max(dhist),
             DIV  = 1.0/_dl_max(dhist),
             ENTR  = _dl_entropy(dhist),
-            TREND = trend(x; kw_d...),
-            LAM  = _laminarity(vhist, rr_v*_rrdenominator(x; kw_v...)),
+            TREND = trend(R; kw_d...),
+            LAM  = _laminarity(vhist, rr_v*_rrdenominator(R; kw_v...)),
             TT   = _vl_average(vhist),
             Vmax = _vl_max(vhist),
             VENTR = _vl_entropy(vhist),
