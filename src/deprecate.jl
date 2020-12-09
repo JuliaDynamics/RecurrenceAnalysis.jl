dcebe0547d0a4f3648db5d311b175a07fb5fb04f
Helios De Rosario <helios.derosario@gmail.com>
2020-12-10 00:16:01 +0100

    new API of rqa with deprecations

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -20,3 +20,44 @@ for call in (:CrossRecurrenceMatrix, :JointRecurrenceMatrix)
     end
 end
 
+function rqa(::Type{NamedTuple}, R; onlydiagonal=false, kwargs...)
+    # Parse arguments for diagonal and vertical structures
+    kw_d = Dict(kwargs)
+    haskey(kw_d, :theilerdiag) && (kw_d[:theiler] = kw_d[:theilerdiag])
+    haskey(kw_d, :lmindiag) && (kw_d[:lmin] = kw_d[:lmindiag])
+    dhist = diagonalhistogram(R; kw_d...)
+    rr_d = recurrencerate(R; kw_d...)
+    if onlydiagonal
+        return (
+            RR  = recurrencerate(R; kwargs...),
+            DET   = _determinism(dhist, rr_d*_rrdenominator(R; kw_d...)),
+            L     = _dl_average(dhist),
+            Lmax  = _dl_max(dhist),
+            DIV   = 1.0/_dl_max(dhist),
+            ENTR  = _dl_entropy(dhist)
+        )
+   else
+        kw_v = Dict(kwargs)
+        haskey(kw_v, :theilervert) && (kw_v[:theiler] = kw_v[:theilervert])
+        haskey(kw_v, :lminvert) && (kw_v[:lmin] = kw_v[:lminvert])
+        vhist, rthist = verticalhistograms(R; kw_v...)
+        rr_v = recurrencerate(R; kw_v...)
+        return (
+            RR  = rr_d,
+            TRANS = transitivity(R),
+            DET  = _determinism(dhist, rr_d*_rrdenominator(R; kw_v...)),
+            L    = _dl_average(dhist),
+            Lmax = _dl_max(dhist),
+            DIV  = 1.0/_dl_max(dhist),
+            ENTR  = _dl_entropy(dhist),
+            TREND = trend(R; kw_d...),
+            LAM  = _laminarity(vhist, rr_v*_rrdenominator(R; kw_v...)),
+            TT   = _vl_average(vhist),
+            Vmax = _vl_max(vhist),
+            VENTR = _vl_entropy(vhist),
+            MRT  = _rt_average(rthist),
+            RTE = _rt_entropy(rthist),
+            NMPRT = maximum(rthist)
+        )
+    end
+end
--- test/deprecations.jl
+++ test/deprecations.jl
@@ -11,4 +11,12 @@ v = Dataset(rand(10, 2))
     @test_logs (:warn, "`JointRecurrenceMatrix(x::AbstractMatrix, y, ε; kwargs...)` is deprecated, use `JointRecurrenceMatrix(Dataset(x), y, ε; kwargs...)`") JointRecurrenceMatrix(xm, v, 0.5)
     @test_logs (:warn, "`JointRecurrenceMatrix(x, y::AbstractMatrix, ε; kwargs...)` is deprecated, use `JointRecurrenceMatrix(x, Dataset(y), ε; kwargs...)`") JointRecurrenceMatrix(v, ym, 0.5)
     @test_logs (:warn, "`JointRecurrenceMatrix(x::AbstractMatrix, y::AbstractMatrix, ε; kwargs...)` is deprecated, use `JointRecurrenceMatrix(Dataset(x), Dataset(y), ε; kwargs...)`") JointRecurrenceMatrix(xm, ym, 0.5)
+    R = RecurrenceMatrix(xm[:,1], 0.5)
+    rqa_params = rqa(R, onlydiagonal=true)
+    @test_logs (:warn, "x.RR is deprecated for results of `rqa`; use x[:RR] instead") rqa_params.RR
+    rqa_tuple = rqa(NamedTuple, R, onlydiagonal=true)
+    @test rqa_params[:RR] == rqa_tuple.RR
+    @test rqa_params[:DET] == rqa_tuple.DET
+    @test rqa_params[:ENTR] == rqa_tuple.ENTR
+    @test rqa_params[:L] == rqa_tuple.L
 end
