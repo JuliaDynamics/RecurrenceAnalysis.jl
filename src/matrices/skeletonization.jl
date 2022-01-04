cb6da54360a076ec2030ca9127011f59b229da17
K.H. Kraemer <hkraemer@pik-potsdam.de>
2022-01-04 13:47:13 +0100

    linehistograms based on vertical lines

--- src/matrices/skeletonization.jl
+++ src/matrices/skeletonization.jl
@@ -4,6 +4,7 @@ are defined.
 =#
 
 # Core function, which gets exported
+
 """
     skeletonize(R) → R_skel
 
@@ -14,53 +15,53 @@ matrix, which only consists of diagonal lines of "thickness" one.
 [^Kraemer2019]: Kraemer, K.H., Marwan, N. (2019). [Border effect corrections for diagonal line based recurrence quantification analysis measures. Physics Letters A 383(34)](https://doi.org/10.1016/j.physleta.2019.125977).
 """
 function skeletonize(X::Union{ARM,SparseMatrixCSC})
-
     if issymmetric(X)
         symm = true
         # convert lower triangle into a close returns map
-        X_cl1 = convert_recurrence_matrix(tril(X))
-        # get "horizontal" line distribution with position indices
-        lines1t, lines1l, lines1c = horizontalhisto(X_cl1)
+        X_cl1 = create_close_returns_map(tril(X))
+        # get "vertical" line distribution with position indices
+        lines1t, lines1l, lines1c = verticalhisto(X_cl1)
         lines_copy1t = deepcopy(lines1t)
         lines_copy1l = deepcopy(lines1l)
         lines_copy1c = deepcopy(lines1c)
 
         # create a close returns map with horizontal lines represented by
         # numbers, equal to their lengths
-        X_hori1 = create_close_returns_map(lines_copy1t, lines_copy1l, lines_copy1c, size(X_cl1))
+        X_vertical1 = create_close_returns_map(lines_copy1t, lines_copy1l, lines_copy1c, size(X_cl1))
+
     else
         symm = false
         # convert upper and lower triangles into close returns maps
-        X_cl1 = convert_recurrence_matrix(tril(X))
-        X_cl2 = convert_recurrence_matrix(sparse(triu(X)'))
+        X_cl1 = create_close_returns_map(tril(X))
+        X_cl2 = create_close_returns_map(sparse(triu(X)'))
 
-        # get "horizontal" line distribution with position indices
-        lines1t, lines1l, lines1c = horizontalhisto(X_cl1)
+        # get "vertical" line distribution with position indices
+        lines1t, lines1l, lines1c = verticalhisto(X_cl1)
         lines_copy1t = deepcopy(lines1t)
         lines_copy1l = deepcopy(lines1l)
         lines_copy1c = deepcopy(lines1c)
-        lines2t, lines2l, lines2c = horizontalhisto(X_cl2)
+        lines2t, lines2l, lines2c = verticalhisto(X_cl2)
         lines_copy2t = deepcopy(lines2t)
         lines_copy2l = deepcopy(lines2l)
         lines_copy2c = deepcopy(lines2c)
 
         # create close returns maps with horizontal lines represented by
         # numbers, equal to their lengths
-        X_hori1 = create_close_returns_map(lines_copy1t, lines_copy1l, lines_copy1c, size(X_cl1))
-        X_hori2 = create_close_returns_map(lines_copy2t, lines_copy2l, lines_copy2c, size(X_cl2))
+        X_vertical1 = create_close_returns_map(lines_copy1t, lines_copy1l, lines_copy1c, size(X_cl1))
+        X_vertical2 = create_close_returns_map(lines_copy2t, lines_copy2l, lines_copy2c, size(X_cl2))
     end
 
     # scan the lines, start with the longest one and discard all adjacent lines
-    lines_final_t, lines_final_l, lines_final_c = get_final_line_matrix(lines1t, lines1l, lines1c, lines_copy1t, lines_copy1l, lines_copy1c, X_hori1)
+    lines_final_t, lines_final_l, lines_final_c = get_final_line_matrix(lines1t, lines1l, lines1c, lines_copy1t, lines_copy1l, lines_copy1c, X_vertical1)
 
     # if not symmetric input RP, than compute for the upper triangle as well
     if !symm
-        lines_final2_t, lines_final2_l, lines_final2_c = get_final_line_matrix(lines2t, lines2l, lines2c, lines_copy2t, lines_copy2l, lines_copy2c, X_hori2)
+        lines_final2_t, lines_final2_l, lines_final2_c = get_final_line_matrix(lines2t, lines2l, lines2c, lines_copy2t, lines_copy2l, lines_copy2c, X_vertical2)
         # build RP based on the histogramm of the reduced lines
-        X_new = build_skeletonized_RP(lines_final_t, lines_final_l, lines_final_c, lines_final2_t, lines_final2_l, lines_final2_c, size(X_hori1,1), size(X_hori1,2))
+        X_new = build_skeletonized_RP(lines_final_t, lines_final_l, lines_final_c, lines_final2_t, lines_final2_l, lines_final2_c, size(X_vertical1,1), size(X_vertical1,2))
     else
         # build RP based on the histogramm of the reduced lines
-        X_new = build_skeletonized_RP(lines_final_t, lines_final_l, lines_final_c, size(X_hori1,1), size(X_hori1,2))
+        X_new = build_skeletonized_RP(lines_final_t, lines_final_l, lines_final_c, size(X_vertical1,1), size(X_vertical1,2))
     end
     return X_new
 end
@@ -68,35 +69,21 @@ end
 # Auxiliary functions
 
 # Transforms the standard RP into a close returns map
-function convert_recurrence_matrix(R::SparseMatrixCSC; triangle::Bool = true)
+function create_close_returns_map(R::SparseMatrixCSC; triangle::Bool = true)
     nr = size(R, 1)
     nrplus = nr+1
     rowvalues = rowvals(R)
     diagvalues = nrplus .+ colvals(R) .- rowvalues # LOI at nrplus (nr+1)
     if triangle
         lower = (diagvalues .<= nrplus)  # indices of the lower triangle
-        return sparse(diagvalues[lower], rowvalues[lower], trues(count(lower)), nrplus, nr)
+        return sparse(rowvalues[lower], diagvalues[lower], trues(count(lower)), nr, nrplus)
     else
-        return sparse(diagvalues, rowvalues, trues(nnz(R)), 2nr+1, nr)
+        return sparse(rowvalues, diagvalues, trues(nnz(R)), nr, 2nr+1)
     end
 end
-
-# Transforms the reverted RP (close returns map) into a normal RP
-function revert_close_returns_map(R::SparseMatrixCSC; triangle::Bool = true)
-    nr = size(R, 2)
-    rowvalues = colvals(R)
-    columnvalues = rowvals(R) .+ rowvalues .- (nr + 1)
-    if triangle
-        lower = (columnvalues .<= rowvalues)
-        return sparse(rowvalues[lower], columnvalues[lower], trues(count(lower)), nr, nr)
-    else
-        return sparse(rowvalues, columnvalues, trues(nnz(R)), nr, nr)
-    end
-end
-
-# from the 3-by-N Matrix, which stores all horizontal lines in the reverted RP,
-# (close returns map) [obtained by calling first `revert_recurrence_matrix` on a
-# recurrence Matrix R, and consecutevely `horizontalhisto(R)`] construct and return
+# from the 3 vectors, which store all horizontal lines in the reverted RP,
+# (close returns map) [obtained by calling first `create_close_returns_map` on a
+# recurrence Matrix R, and consecutevely `verticalhisto(R)`] construct and return
 # a "dummy" close returns map, where all lines are encoded according to their length.
 function create_close_returns_map(lines1, lines2, lines3, dims)
     nzv = sum(lines1)
@@ -106,20 +93,36 @@ function create_close_returns_map(lines1, lines2, lines3, dims)
     startspan = 0
     for (i, val) in enumerate(lines1)
         span = startspan .+ (1:val)
-        rowvalues[span] .= lines2[i]
-        colvalues[span] .= lines3[i]-1 .+ (1:val)
+        colvalues[span] .= lines3[i]
+        rowvalues[span] .= lines2[i]-1 .+ (1:val)
         lenvalues[span] .= val
         startspan += val
     end
     return sparse(rowvalues, colvalues, lenvalues, dims...)
 end
 
+# Transforms the reverted RP (close returns map) into a normal RP
+function revert_close_returns_map(R::SparseMatrixCSC; triangle::Bool = true)
+    nr = size(R, 1)
+    # rowvalues = colvals(R)
+    # columnvalues = rowvals(R) .+ rowvalues .- (nr + 1)
+    rowvalues = rowvals(R)
+    columnvalues = rowvals(R) .+ colvals(R) .- (nr + 1)
+    if triangle
+        lower = (columnvalues .<= rowvalues)
+        return sparse(rowvalues[lower], columnvalues[lower], trues(count(lower)), nr, nr)
+    else
+        return sparse(rowvalues, columnvalues, trues(nnz(R)), nr, nr)
+    end
+end
+
+
 # deletes a line, specified in 'l_vec' (line vector, with first line being
 # the total line length, the second line the line-index of the starting point
 # and the third line the column-index of the starting point) from the close
 # returns "RP" 'R'.
 function delete_line_from_cl_ret_RP!(R::Union{ARM,SparseMatrixCSC}, line1::Int, line2::Int, line3::Int)
-    R[line2, line3 .+ (1:line1).-1] .= 0
+    R[line2 .+ (1:line1).-1 , line3] .= 0
 end
 
 # build the skeletonized RP from the line-Matrix
@@ -132,7 +135,7 @@ function build_skeletonized_RP(lines1::Vector{Int}, lines2::Vector{Int}, lines3:
         linei = lines2[i]
         columni = lines3[i]
         for j = 1:l_max
-            X_cl_new[linei, columni+j-1] = true
+            X_cl_new[linei+j-1, columni] = true
         end
     end
     XX = revert_close_returns_map(X_cl_new) # revert this close returns map into a legal RP
@@ -152,7 +155,7 @@ function build_skeletonized_RP(lines1::Vector{Int}, lines2::Vector{Int}, lines3:
         linei = lines2[i]
         columni = lines3[i]
         for j = 1:l_max
-            X_cl_new[linei, columni+j-1] = true
+            X_cl_new[linei+j-1, columni] = true
         end
     end
     @inbounds for i = 1:length(lines11)
@@ -160,7 +163,7 @@ function build_skeletonized_RP(lines1::Vector{Int}, lines2::Vector{Int}, lines3:
         linei = lines22[i]
         columni = lines33[i]
         for j = 1:l_max
-            X_cl2_new[linei,columni+j-1] = true
+            X_cl2_new[linei+j-1,columni] = true
         end
     end
 
@@ -174,9 +177,9 @@ end
 
 function get_final_line_matrix(lines1t::Vector{Int}, lines1l::Vector{Int},
     lines1c::Vector{Int}, lines_copy1t::Vector{Int}, lines_copy1l::Vector{Int},
-    lines_copy1c::Vector{Int}, X_hori1::SparseMatrixCSC)
+    lines_copy1c::Vector{Int}, X_vertical1::SparseMatrixCSC)
 
-    N, M = size(X_hori1)
+    N, M = size(X_vertical1)
     # initialize final lines
     lines_final_t = Int[]
     lines_final_l = Int[]
@@ -198,7 +201,7 @@ function get_final_line_matrix(lines1t::Vector{Int}, lines1l::Vector{Int},
         push!(lines_final_l, lines1l[l_ind])
         push!(lines_final_c, lines1c[l_ind])
         # delete this line from the RP
-        delete_line_from_cl_ret_RP!(X_hori1, lines1t[l_ind], lines1l[l_ind], lines1c[l_ind])
+        delete_line_from_cl_ret_RP!(X_vertical1, lines1t[l_ind], lines1l[l_ind], lines1c[l_ind])
         # go along each point of the line and check for neighbours
         l_max = lines1t[l_ind]
 
@@ -206,27 +209,27 @@ function get_final_line_matrix(lines1t::Vector{Int}, lines1l::Vector{Int},
             # scan each line twice - above and underneth
             for index = -1:2:1
                 # make sure not to exceed RP-boundaries
-                (linei+index > N || linei+index == 0) ? break : nothing
+                (columni+index > M || columni+index == 0) ? break : nothing
                 # if there is a neighbouring point, call recursive scan-function
-                (X_hori1[linei+index, columni+l-1] != 0) ? scan_lines!(X_hori1, lines_copy1t,
-                                lines_copy1l, lines_copy1c, linei+index, columni+l-1) : nothing
+                (X_vertical1[linei+l-1, columni+index] != 0) ? scan_lines!(X_vertical1, lines_copy1t,
+                                lines_copy1l, lines_copy1c, linei+l-1, columni+index) : nothing
             end
         end
     end
     return lines_final_t, lines_final_l, lines_final_c
 end
 
-# compute a `3-by-N` matrix, which stores all horizontal lines from a converted
-# recurrence matrix (after applying `convert_recurrence_matrix` to a ARM). The
+# compute a `3-by-N` matrix, which stores all vertical lines from a converted
+# recurrence matrix (after applying `create_close_returns_map` to an ARM). The
 # line lengths of each found line are stored in the first row, and the starting
 # indices of its row and column, are stored in the 2nd and 3rd row, respectively.
-function horizontalhisto(R::SparseMatrixCSC)
-
-    N = size(R)[1]
+# Returns the lines of this sorted matrix as vectors
+function verticalhisto(R::SparseMatrixCSC)
+    N = size(R)[2]
     liness = zeros(Int, 3, 1)
     @inbounds for j = 1:N
-        starts = findall(diff([0; @view R[j,:]]).==1)
-        ends = findall(diff([@view R[j,:]; 0]).==-1)
+        starts = findall(diff([0; @view R[:,j]]).==1)
+        ends = findall(diff([@view R[:,j]; 0]).==-1)
 
         if ~isempty(starts)
             lines = zeros(Int, 3, length(starts))
@@ -243,15 +246,11 @@ function horizontalhisto(R::SparseMatrixCSC)
     liness = liness[:, no_zero_lines]
     ls = size(liness)
     liness = liness[:,sortperm(@view liness[1, :]; rev=true)]
-    return vec(liness[1,:]), vec(liness[2,:]), vec(liness[3,:])
+    return vec(liness[1,:]), vec(liness[3,:]), vec(liness[2,:])
 end
-
-# function horizontalhisto2(R::SparseMatrixCSC)
-#     # Transpose R in order to get the "horizontal" lines
-#     # rows = colvals(R[2:end,:])
-#     # cols = rowvals(R[2:end,:])
-#     rows = colvals(R)
-#     cols = rowvals(R)
+# function verticalhisto2(R::SparseMatrixCSC)
+#     rows = rowvals(R)
+#     cols = colvals(R)
 #     p = sortperm(cols)
 #     rows = rows[p]
 #     cols = cols[p]
@@ -297,13 +296,13 @@ end
 #     liness[1,end] = current_vert
 #     # process the first dummy-fragment
 #     liness[1,1] = 0
-
+#
 #     # remove lines of length zero (=no line)
 #     no_zero_lines = findall(liness[1,:].!=0)
 #     liness = liness[:, no_zero_lines]
 #     ls = size(liness)
 #     liness = liness[:,sortperm(@view liness[1, :]; rev=true)]
-#     return vec(liness[1,:]), vec(liness[2,:]), vec(liness[3,:])
+#     return vec(liness[1,:]), vec(liness[3,:]), vec(liness[2,:])
 # end
 
 # manipulates XX inplace and returns a view on l_vec
@@ -315,8 +314,8 @@ function scan_lines!(XX::SparseMatrixCSC, l_vec1::AbstractVector{<:Integer}, l_v
     while true
         # check whether the input index tuple is a listed index for starting
         # points of line lengths in the line matrix
-        loc_line = findall(line .== l_vec2)
-        found = findfirst(isequal(column+index), l_vec3[loc_line])
+        loc_line = findall(column .== l_vec3)
+        found = findfirst(isequal(line+index), l_vec2[loc_line])
         if found !== nothing
             del_ind = loc_line[found]
             break
@@ -344,8 +343,8 @@ end
 
 function neighborindices(XX, i::T, li::T, co::T) where T<:Integer
     N, M = size(XX)
-    for newli in (li-1, li+1), newco in (co+i .+ (-2:0))
-        if (1 ≤ newli ≤ N) && (newco != 0) && (newco ≤ M)
+    for newli in (li+i .+ (-2:0)), newco in (co-1, co+1)
+        if (1 ≤ newli ≤ N) && (1 ≤ newco ≤ M)
             (XX[newli, newco] != 0) && return (newli, newco)
         end
     end
