4630ae1276143580747d889cd483bc9b2de97da9
Alex Manaenkov <allomulder@gmail.com>
2023-03-03 13:43:15 +0300

    Revert formatting

--- src/rqa/windowed_function.jl
+++ src/rqa/windowed_function.jl
@@ -6,14 +6,14 @@ to windowed views of the given recurrence matrix `rmat` with given window `width
 and `step`. The `kwargs...` are propagated to the call `f(rmat_view; kwargs...)`.
 """
 function windowed(rmat, f, width, step=1; kwargs...)
-  windows = 1:step:(size(rmat, 1)-width)
-  map(1:length(windows)) do i
-    rmat_view = view(
-      rmat,
-      windows[i]:(windows[i]+width),
-      windows[i]:(windows[i]+width)
-    )
-    f(rmat_view; kwargs...)
-  end
+    windows = 1:step:(size(rmat, 1)-width)
+    map(1:length(windows)) do i
+      rmat_view = view(
+        rmat,
+        windows[i]:(windows[i]+width),
+        windows[i]:(windows[i]+width)
+      )
+      f(rmat_view; kwargs...)
+    end
 end
 
