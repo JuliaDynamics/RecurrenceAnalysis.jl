a9aa0a5abf2e121eb3e847077a4ffe6931da9651
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-10 14:42:50 -0400

    move to workers?
    the runtime with threads is just outrageous...not sure why

--- src/matrices/recurrence_matrix_low.jl
+++ src/matrices/recurrence_matrix_low.jl
@@ -60,6 +60,21 @@ end
 # Now, we define the parallel versions of these functions.
 
 # Core function
+
+function _full_task_worker!(rowvals, colvals, x::Vector_or_SSSet, y::Vector_or_SSSet, k, metric::Metric, ε::E) where E
+    for j in k
+        nzcol = 0
+        for i in eachindex(x)
+            @inbounds if evaluate(metric, x[i], y[j]) ≤ ( (ε isa Real) ? ε : ε[j] )
+                push!(rowvals, i) # push to the thread-specific row array
+                nzcol += 1
+            end
+        end
+        append!(colvals, fill(j, (nzcol,)))
+    end
+    return nothing
+end
+
 function recurrence_matrix(x::Vector_or_SSSet, y::Vector_or_SSSet, metric::Metric, ε, ::Val{true})
     @assert ε isa Real || length(ε) == length(y)
     # We create an `Array` of `Array`s, for each task to have its
@@ -68,38 +83,47 @@ function recurrence_matrix(x::Vector_or_SSSet, y::Vector_or_SSSet, metric::Metri
 
     # For load balancing reasons, we will use 2 tasks per CPU thread that we have
     # available.
-    ntasks = Threads.nthreads() * 2
+    ntasks = Threads.nthreads()
+    chunks = ntuple(ntasks) do ichunk
+        # Adapted from JuliaFolds2/ChunkSplitters.jl, MIT license, credit to the authors.
+        n_per_chunk, n_remaining = divrem(length(y), ntasks)
+        first = 1 + (ichunk - 1) * n_per_chunk + ifelse(ichunk <= n_remaining, ichunk - 1, n_remaining)
+        last = (first - 1) + n_per_chunk + ifelse(ichunk <= n_remaining, 1, 0)
+        first:last
+    end
+
     rowvals = [Vector{Int}() for _ in 1:ntasks]
     colvals = [Vector{Int}() for _ in 1:ntasks]
 
     # This is the same logic as the serial function, but parallelized.
     # We create all tasks in this array comprehension so we have them stored,
     # but they are launched at the same time approximately.
-    # 
     tasks = [
-        Threads.@spawn begin
-            for j in eachindex(y)
-                taskn = $i
-                nzcol = 0
-                for i in eachindex(x)
-                    @inbounds if evaluate(metric, x[i], y[j]) ≤ ( (ε isa Real) ? ε : ε[j] )
-                        push!(rowvals[taskn], i) # push to the thread-specific row array
-                        nzcol += 1
-                    end
-                end
-                append!(colvals[taskn], fill(j, (nzcol,)))
-            end
-        end
-        for i in 1:ntasks
+        Threads.@spawn _full_task_worker!($(rowvals[i]), $(colvals[i]), $(x), $(y), $(k), $(metric), $(ε))
+        for (i, k) in enumerate(chunks)
     ]
     # The array comprehension above scheduled the tasks...now we have to wait on them to make
     # sure they are all complete before we access the results.
-    foreach(wait, tasks)
+    foreach(fetch, tasks)
     # Now that we know all tasks are complete, we can merge the results.
     finalrows = reduce(vcat, rowvals) # merge into one array
     finalcols = reduce(vcat, colvals) # merge into one array
     nzvals = fill(true, (length(finalrows),))
     return sparse(finalrows, finalcols, nzvals, length(x), length(y))
+end
+
+function _lower_triangular_task_worker!(rowvals, colvals, x::Vector_or_SSSet, k, metric::Metric, ε::E) where E
+    for j in k
+        nzcol = 0
+        for i in 1:j
+            @inbounds if evaluate(metric, x[i], x[j]) ≤ ( (ε isa Real) ? ε : ε[j] )
+                push!(rowvals, i) # push to the thread-specific row array
+                nzcol += 1
+            end
+        end
+        append!(colvals, fill(j, (nzcol,)))
+    end
+    return nothing
 end
 
 function recurrence_matrix(x::Vector_or_SSSet, metric::Metric, ε, ::Val{true})
@@ -108,29 +132,17 @@ function recurrence_matrix(x::Vector_or_SSSet, metric::Metric, ε, ::Val{true})
     # own array to push to.  This avoids race conditions with
     # multiple threads pushing to the same `Array` (`Array`s are not atomic).
 
-    ntasks = Threads.nthreads() * 2
+    ntasks = Threads.nthreads()
 
     rowvals = [Vector{Int}() for _ in 1:ntasks]
     colvals = [Vector{Int}() for _ in 1:ntasks]
 
     # This is the same logic as the serial function, but parallelized.
     tasks = [
-        Threads.@spawn begin
-            threadn = $i # note the `$` that does "interpolation" into the task
-            for j in $k
-                nzcol = 0
-                for i in 1:j
-                    @inbounds if evaluate(metric, x[i], x[j]) ≤ ( (ε isa Real) ? ε : ε[j] )
-                        push!(rowvals[threadn], i) # push to the thread-specific row array
-                        nzcol += 1
-                    end
-                end
-                append!(colvals[threadn], fill(j, (nzcol,)))
-            end
-        end
+        Threads.@spawn _lower_triangular_task_worker!($(rowvals[i]), $(colvals[i]), $(x), $(k), $(metric), $(ε))
         for (i, k) in enumerate(partition_indices(length(x), ntasks))
     ]
-    foreach(wait, tasks)
+    foreach(fetch, tasks)
     finalrows = reduce(vcat, rowvals) # merge into one array
     finalcols = reduce(vcat, colvals) # merge into one array
     nzvals = fill(true, (length(finalrows),))
